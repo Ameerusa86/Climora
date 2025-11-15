a2610d6eacd47dda374b4732030b79e84e7a8e70
Ameer Hasan <ameer.hasan.dev@gmail.com>
2025-11-15 17:42:08 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
-import { getWeather } from "./api";
+import AdditionalInfo from "./components/cards/AdditionalInfo";
+import CurrentWeather from "./components/cards/CurrentWeather";
+import DailyForecast from "./components/cards/DailyForecast";
+import HourlyForecast from "./components/cards/HourlyForecast";
 
 function App() {
-  const { data } = useQuery({
-    queryKey: ["weather"],
-    queryFn: () => getWeather(40.7128, -74.006), // Example coordinates for New York City
-  });
+  // const { data } = useQuery({
+  //   queryKey: ["weather"],
+  //   queryFn: () => getWeather(10, 25), // Example coordinates for New York City
+  // });
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div className="flex flex-col gap-8">
+      <CurrentWeather coords={{ lat: 10, lon: 25 }} />
+      <HourlyForecast coords={{ lat: 10, lon: 25 }} />
+      <DailyForecast coords={{ lat: 10, lon: 25 }} />
+      <AdditionalInfo coords={{ lat: 10, lon: 25 }} />
+    </div>
+  );
 }
 
 export default App;
